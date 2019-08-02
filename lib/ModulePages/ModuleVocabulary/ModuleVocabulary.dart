24c78691db84a6822ba2c6a3adcd63caddd077a6
Subramanya <subramanyarao4@gmail.com>
2019-08-02 18:52:55 +0530

    Gradient added for timeline

--- lib/ModulePages/ModuleVocabulary/ModuleVocabulary.dart
+++ lib/ModulePages/ModuleVocabulary/ModuleVocabulary.dart
@@ -70,7 +70,7 @@ class _ModuleVocabularyState extends State<ModuleVocabulary> {
                           Image.asset(
                             "assets/Images/vocabulary.png",
                             fit: BoxFit.fitHeight,
-                            height: 200,
+//                            height: 200,
                           ),
                           TypewriterAnimatedTextKit(
                               onTap: () {
