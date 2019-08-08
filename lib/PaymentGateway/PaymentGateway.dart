cf493d5844dd1581bb8d293a4c6c1e8322c293fd
Subramanya <subramanyarao4@gmail.com>
2019-08-08 17:39:12 +0530

    discussion board added

--- lib/PaymentGateway/PaymentGateway.dart
+++ lib/PaymentGateway/PaymentGateway.dart
@@ -87,24 +87,12 @@ class _PaymentGatewayPageState extends State<PaymentGatewayPage> with AutomaticK
 
 
   Future<Null> _payment() async {
-<<<<<<< HEAD
-     bool result = await platform.invokeMethod('payment');
-     print("Message from PaymentGateway.dart RESULT IS VAL IS $result");
-//     if(result){
-//       var data = Map<String,dynamic>();
-//       data["payment"]=true;
-//       await db.collection("user").document(userId).setData(data);
-//     }
-//     else{
-//        print("hey from payment gateway");
-//     }
-=======
+
      await platform.invokeMethod('payment');
     //  print("Message from PaymentGateway.dart RESULT IS VAL IS  $result");
      var data = Map<String,dynamic>();
      data["payment"]=true;
      await db.collection("user").document(userId).setData(data);
->>>>>>> 72f23f145e355127fb0cebd7a72ab8a016a220fe
   }
 
 
